2b01ac0dcbf229d97b828e814c99a41eb86b8a1a
Md. Emran Hossen <92653363+emmranhossen@users.noreply.github.com>
2024-02-08 23:09:15 +0600

    Add files via upload

---
+++ Valentine_Gifts.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while(t--) {
+	    int x; cin >> x;
+	    if (x < 127) {
+	        cout << "NO" << '\n';
+	    }
+	    else cout << "YES" << '\n';
+	}
+
+}
